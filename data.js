2ca38b3363897967500e2d3ff2a792808ab3dad1
Lovecraft255 <dilanbemitez6@gmail.com>
2022-04-09 20:31:15 -0300

    Data exportada al JS principal

---
+++ data.js
@@ -0,0 +1,105 @@
+const data = [
+
+
+    {
+
+        question : "¿Cual de estos videojuegos no es de FromSoftware?",
+
+        answers : ["Dark Souls" , "Sekiro" , "Doom" , "Elden Ring"],
+
+        correctAnswer : 2
+
+    },
+
+    {
+
+        question : "¿A que banda le pertenece la cancion 'Killing in the name'?",
+
+        answers : ["Slipknot" , "Korn" , "Nirvana" , "Rage against the machine"],
+
+        correctAnswer : 3
+
+    },
+
+    {
+
+        question : "¿En que año murio el cantante de Nirvana, Kurt Cobain?",
+
+        answers : ["1994" , "1991" , "1987" , "1970"],
+
+        correctAnswer : 0
+
+    },
+
+    {
+
+        question : "¿En que lenguaje de programacion esta hecho el Minecraft?",
+
+        answers : ["SQL" , "Java" , "C++" , "PHP"],
+
+        correctAnswer : 1
+
+    },
+
+    {
+
+        question : "¿Quien es el protagoinista del anime 'Akira'?",
+
+        answers : ["Kei" , "Tetsuo" , "Kaneda" , "Akira"],
+
+        correctAnswer : 2
+
+    },
+
+    {
+
+        question : "¿Cual de estos personajes es hijo de Virgil en Devil May Cry?",
+
+        answers : ["Esparda" , "Lady" , "Dante" , "Nero"],
+
+        correctAnswer : 3
+
+    },
+
+    {
+
+        question : "¿Cuantos hijos tiene Yujiro en Baki?",
+
+        answers : ["2" , "ninguno" , "4" , "1"],
+
+        correctAnswer : 0
+
+    },
+
+    {
+
+        question : "¿Cual de estos juegos fue el ganador del GOTY del 2015?",
+
+        answers : ["Bloodborne", "Fallout 4", "Metal Gear Solid V: The Phantom Pain", "The Witcher 3"],
+
+        correctAnswer : 3
+
+    },
+
+    {
+
+        question : "¿Cual de estas pelicualas no es de Pixar?",
+
+        answers : ["Los Mitchell vs las maquinas" , "Red" , "Toy Story 4" , "Coco"],
+
+        correctAnswer : 0
+
+    },
+
+    {
+
+        question : "¿Cual de estas peliculas no es del estudio Ghibli?",
+
+        answers : ["El increible castillo del vagabundo" , "El viaje de Chihiro" , "Mi vecino Totoro" , "Una voz silenciosa"],
+
+        correctAnswer : 3
+
+    }
+
+]
+export {data};
