3e5a54f4e77f67b8b475c1ceeb0916561d91ef0f
Lovecraft255 <dilanbemitez6@gmail.com>
2022-04-10 14:28:05 -0300

    intro.html terminado y localstrage usado para el username

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 import {data} from "./data.js"
 
+if(window.location.pathname == "/intro.html"){
+
+    let startBtn = document.getElementById("startBtn");
+
+    console.log(startBtn);
+    
+
+    startBtn.addEventListener( "click" , () => {
+
+        let userName = document.getElementById("userName").value
+
+        if(userName == "" || userName == null){
+            alert("No se ingreso nombre de usuarion");
+            console.log(userName);
+        }else{
+            console.log(userName);
+            localStorage.setItem( "UserName" , JSON.stringify(userName) );
+            window.location.replace("/quest.html");
+        }
+
+    });
+
+}
+
+if(window.location.pathname == "/quest.html"){
+
+    let questionDiv = document.getElementById("question");
+    let answersDiv = document.getElementById("answers");
+    console.log( questionDiv );
+
+    console.log( answersDiv );
+    
+}
