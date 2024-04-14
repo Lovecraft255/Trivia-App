7df76b00af58b5a9e274fc394924e7e6b8397408
Lovecraft255 <dilanbemitez6@gmail.com>
2024-04-14 19:45:10 -0300

    Ruta mejorada 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { data } from "./data.js"
 
-if (window.location.pathname == "./index.html") {
+if (window.location.pathname == "index.html") {
 
     let startBtn = document.getElementById("startBtn");
 
